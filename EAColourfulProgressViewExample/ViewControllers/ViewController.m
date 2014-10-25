8a984252246a0125269b50f7b28c1a1e2696ca37
Eddpt <Eddpt@users.noreply.github.com>
2014-10-25 09:42:59 +0100

    Fixed documentation references

--- EAColourfulProgressViewExample/ViewControllers/ViewController.m
+++ EAColourfulProgressViewExample/ViewControllers/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  EAColourfulProgressViewExample
 //
-//  Created by Edgar Antunes on 25/10/2014.
+//  Created by Eddpt on 25/10/2014.
 //  Copyright (c) 2014 xpto. All rights reserved.
 //
 
