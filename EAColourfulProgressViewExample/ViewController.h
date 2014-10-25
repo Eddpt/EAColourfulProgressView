21d46954465bf59074f3ef6c74db7451f6fb5957
Edgar Antunes <edgar@theappbusiness.co.uk>
2014-10-25 09:23:03 +0100

    Initial Commit

---
+++ EAColourfulProgressViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EAColourfulProgressViewExample
+//
+//  Created by Edgar Antunes on 25/10/2014.
+//  Copyright (c) 2014 xpto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
